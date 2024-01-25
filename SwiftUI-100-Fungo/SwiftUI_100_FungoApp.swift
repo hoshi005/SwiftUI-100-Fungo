35237a1045183a0af737adbc6ea3eadfcf4d1136
Susumu Hoshikawa <hoshi005@me.com>
2024-01-25 18:41:09 +0900

    initial commit

---
+++ SwiftUI-100-Fungo/SwiftUI_100_FungoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_100_FungoApp.swift
+//  SwiftUI-100-Fungo
+//
+//  Created by Susumu Hoshikawa on 2024/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_100_FungoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
