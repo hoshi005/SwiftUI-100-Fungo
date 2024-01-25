9e05af3c2f3f2f648267db8cce20ff8f0483ff4d
Susumu Hoshikawa <hoshi005@me.com>
2024-01-25 19:10:38 +0900

    001

--- SwiftUI-100-Fungo/Views/View001.swift
+++ SwiftUI-100-Fungo/Views/View001.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct View001: View {
     var body: some View {
-        Text("001")
+        Image(.icon)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 150, height: 200)
+            .background(Color.red)
     }
 }
 
