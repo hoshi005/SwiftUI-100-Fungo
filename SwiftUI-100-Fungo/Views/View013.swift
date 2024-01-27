e5895d186ae6592528a76041c04a9473fb4bd62c
Susumu Hoshikawa <hoshi005@me.com>
2024-01-27 16:05:12 +0900

    013

--- SwiftUI-100-Fungo/Views/View013.swift
+++ SwiftUI-100-Fungo/Views/View013.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct View013: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section("normal") {
+                Text("Normal 1")
+                Text("Normal 2")
+                Text("Normal 3")
+            }
+            Section("free") {
+                Text("Free 1")
+                Text("Free 2")
+                Text("Free 3")
+            }
+        }
+        .listStyle(.plain)
+        .navigationTitle("13. Listのスタイルを変更する")
     }
 }
 
