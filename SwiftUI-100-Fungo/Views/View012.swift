fb7f0e8a9a40077e4bb07c988c905c7d6215d99e
Susumu Hoshikawa <hoshi005@me.com>
2024-01-27 15:58:55 +0900

    先に020までファイルを追加

---
+++ SwiftUI-100-Fungo/Views/View012.swift
@@ -0,0 +1,18 @@
+//
+//  View012.swift
+//  SwiftUI-100-Fungo
+//
+//  Created by Susumu Hoshikawa on 2024/01/27.
+//
+
+import SwiftUI
+
+struct View012: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View012()
+}
---
+++ SwiftUI-100-Fungo/Views/View018.swift
@@ -0,0 +1,18 @@
+//
+//  View018.swift
+//  SwiftUI-100-Fungo
+//
+//  Created by Susumu Hoshikawa on 2024/01/27.
+//
+
+import SwiftUI
+
+struct View018: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View018()
+}
