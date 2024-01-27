ea84316719be65cc676c85403a9ae3cbc3c8efe7
Susumu Hoshikawa <hoshi005@me.com>
2024-01-27 16:42:57 +0900

    017

--- SwiftUI-100-Fungo/Views/View017.swift
+++ SwiftUI-100-Fungo/Views/View017.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
+// 数値の入力チェックは、Int()かDouble()で戻り値を確認すると良い
+
 struct View017: View {
+    @State private var isShowAlert = false
+    @State private var isShowSheet = false
+    @State private var text = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("input number", text: $text)
+                .textFieldStyle(.roundedBorder)
+            Button("Show !!") {
+                if let _ = Int(text) {
+                    isShowSheet.toggle()
+                } else {
+                    isShowAlert.toggle()
+                }
+            }
+        }
+        .padding()
+        .alert("数値以外が入力されたよ", isPresented: $isShowAlert) {
+            Button("OK", role: .none) {}
+            Button("Cancel", role: .cancel) {}
+        }
+        .sheet(isPresented: $isShowSheet) {
+            Text("数値が入力されたよ")
+        }
     }
 }
 
