97a461939d4141a499fd1afd14ca6fe188c641bc
Susumu Hoshikawa <hoshi005@me.com>
2024-01-30 21:08:06 +0900

    020

--- SwiftUI-100-Fungo/Views/View020.swift
+++ SwiftUI-100-Fungo/Views/View020.swift
@@ -7,9 +7,77 @@
 
 import SwiftUI
 
+private let text = """
+I❤️ Snorlax!
+Snorlax is most cute & cool &
+powerful. Pokemon.
+"""
+
 struct View020: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Spacer()
+            
+            Text(attributedText())
+            
+            Spacer()
+            
+            Text("I")
+                .font(.system(size: 20).bold())
+            + Text(" ❤️ ")
+                .foregroundStyle(Color.red)
+            + Text("Snorlax")
+                .foregroundStyle(Color.black)
+                .font(.system(size: 20).bold())
+            + Text("!\n")
+            + Text("Snorlax ")
+                .foregroundStyle(Color.red)
+                .font(.system(size: 20).bold())
+            + Text("is most ")
+                .foregroundStyle(Color.blue)
+                .font(.system(size: 20).bold())
+            + Text("cute & cool& powerful .")
+                .foregroundStyle(Color.green)
+                .font(.system(size: 20).bold())
+            + Text("Pokemon.")
+                .foregroundStyle(Color.yellow)
+                .font(.system(size: 20).bold())
+            
+            Spacer()
+        }
+    }
+    
+    func attributedText() -> AttributedString {
+        var attrText = AttributedString(text)
+        
+        if let range = attrText.range(of: "Snorlax ") {
+            attrText[range].foregroundColor = .red
+            attrText[range].font = .system(size: 18, weight: .bold)
+        }
+        
+        if let range = attrText.range(of: "is most") {
+            attrText[range].foregroundColor = .blue
+            attrText[range].font = .system(size: 18, weight: .bold)
+        }
+        
+        if let range = attrText.range(of: "cute & cool &") {
+            attrText[range].foregroundColor = .green
+            attrText[range].font = .system(size: 18, weight: .bold)
+        }
+        
+        if let range = attrText.range(of: "powerful.") {
+            attrText[range].foregroundColor = .green
+            attrText[range].font = .system(size: 18, weight: .bold)
+        }
+        
+        if let range = attrText.range(of: "Pokemon.") {
+            attrText[range].foregroundColor = .yellow
+            attrText[range].font = .system(size: 18, weight: .bold)
+        }
+        
+        return attrText
     }
 }
 
