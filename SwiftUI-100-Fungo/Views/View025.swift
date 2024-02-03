fa3e7a67ab01f5ada2e0a173ea7c7fbb954f819b
Susumu Hoshikawa <hoshi005@me.com>
2024-02-03 13:13:28 +0900

    025

--- SwiftUI-100-Fungo/Views/View025.swift
+++ SwiftUI-100-Fungo/Views/View025.swift
@@ -7,12 +7,72 @@
 
 import SwiftUI
 
+// 関数をそのまま渡してもいいが、delegateとしてインスタンスをそのまま渡すなど
+
 struct View025: View {
+    @State private var isNext = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Button("Button1") {
+                isNext.toggle()
+            }
+            .navigationDestination(isPresented: $isNext) {
+                NextView025(onTap: hoge)
+            }
+        }
+    }
+    
+    func hoge() {
+        print("hoge")
     }
 }
 
 #Preview {
     View025()
 }
+
+struct NextView025: View {
+    
+    let onTap: () -> Void
+    
+    var body: some View {
+        Button(action: {
+            onTap()
+        }, label: {
+            Text("Button2")
+        })
+    }
+}
+
+
+
+//import SwiftUI
+//
+//struct ContentView: View, MyProtocol {
+//    @State var text: String = "My Text"
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                Text(text)
+//                NavigationLink(destination: SecondView(delegate: self)) {
+//                    Text("2nd View")
+//                }
+//            }
+//        }
+//    }
+//
+//    func myFunc() {
+//        text = "Changed Text"
+//    }
+//}
+//
+//struct SecondView: View {
+//    var delegate: MyProtocol
+//    var body: some View {
+//        Button(action: {
+//            self.delegate.myFunc()
+//        }) {
+//            Text("ChangeText")
+//        }
+//    }
+//}
