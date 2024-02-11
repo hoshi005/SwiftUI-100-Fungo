1eef19b2414287bb1cac23f459d8b3f4cdcb4240
Susumu Hoshikawa <hoshi005@me.com>
2024-02-11 16:00:56 +0900

    027

--- SwiftUI-100-Fungo/Views/View027.swift
+++ SwiftUI-100-Fungo/Views/View027.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct View027: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { geometry in
+            HStack {
+                Picker(selection: .constant(0)) {
+                    ForEach(0..<10, id: \.self) { value in
+                        Text("\(value)")
+                    }
+                } label: {
+                    Text("AAA")
+                }
+                .labelsHidden()
+                .pickerStyle(.wheel)
+                .frame(
+                    width: geometry.size.width / 2,
+                    height: geometry.size.height
+                )
+                .clipped()
+                
+                Picker(selection: .constant(0)) {
+                    ForEach(10..<20, id: \.self) { value in
+                        Text("\(value)")
+                    }
+                } label: {
+                    Text("AAA")
+                }
+                .labelsHidden()
+                .pickerStyle(.wheel)
+                .frame(
+                    width: geometry.size.width / 2,
+                    height: geometry.size.height
+                )
+                .clipped()
+            }
+        }
+        .padding()
     }
 }
 
